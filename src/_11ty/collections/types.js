fe1f71d8fb47cb55cb8a40e04d47a2b0075938fb
Nicolas Hoizey <nicolas@hoizey.com>
2020-04-14 09:36:30 +0200

    Static navigation, KISS

--- src/_11ty/collections/types.js
+++ src/_11ty/collections/types.js
@@ -9,6 +9,17 @@ contentTypes.forEach((type) => {
 if (contentTypes.length > 0) {
   collections['contents'] = (collection) =>
     getFilteredCollection(collection, `{${contentTypes.join(',')}}`);
+  // collections['types'] = (collection) =>
+  //   getFilteredCollection(collection, `{${contentTypes.join(',')}}`).filter(
+  //     (item) => {
+  //       console.log(item.filePathStem);
+  //       let matches = item.filePathStem.match(/^\/([^\/]+)\/index$/);
+  //       console.dir(
+  //         matches !== null && contentTypes.indexOf(matches[1]) !== -1
+  //       );
+  //       return matches !== null && contentTypes.indexOf(matches[1]) !== -1;
+  //     }
+  //   );
 }
 
 module.exports = collections;
