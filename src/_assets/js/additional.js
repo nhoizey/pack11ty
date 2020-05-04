b3a53165f5ae2536ecdec11f46fc67ae58b61a48
Nicolas Hoizey <nicolas@hoizey.com>
2020-05-05 01:47:37 +0200

    Add a wrapper around content tables to make them scrollable

--- src/_assets/js/additional.js
+++ src/_assets/js/additional.js
@@ -1,6 +1,21 @@
 // Get additional CSS for Rollup
 import '../sass/additional.scss';
 
+// Add horizontal scroll to overflowing content tables
+window.addEventListener('load', () => {
+  document.querySelectorAll('main table').forEach((table) => {
+    // create wrapper container
+    let wrapper = document.createElement('div');
+    wrapper.style.overflowX = 'scroll';
+
+    // insert wrapper before el in the DOM tree
+    table.parentNode.insertBefore(wrapper, table);
+
+    // move table into wrapper
+    wrapper.appendChild(table);
+  });
+});
+
 // Install Service Worker
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
