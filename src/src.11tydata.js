7a7cd90afbf652fb147c371bd064fefac9ce0768
Nicolas Hoizey <nicolas@hoizey.com>
2020-05-05 01:48:55 +0200

    No need for a separated function

--- src/src.11tydata.js
+++ src/src.11tydata.js
@@ -1,25 +1,31 @@
 const fs = require('fs');
-
-const type = (data) => {
-  let type = null;
-  // Let's find if this content is in a root folder without '_' prefix
-  let matches = data.page.inputPath.match(/^\.\/src\/([^_][^/]+)\/.*$/);
-  if (matches) {
-    folder = matches[1];
-    if (fs.existsSync(`src/_layouts/${folder}.njk`)) {
-      type = folder;
-    } else {
-      type = 'pages';
-    }
-  }
-  return type;
-};
 const config = require('../pack11ty.config.js');
 
 module.exports = {
   lang: config.defaultLang || 'en',
   eleventyComputed: {
-    layout: (data) => type(data),
+    layout: (data) => {
+      if (data.layout !== undefined && data.layout !== '') {
+        // A layout has been set in the content Front Matter
+        return data.layout;
+      }
+
+      // Default layout is a page
+      let layout = 'pages';
+
+      // Let's find if this content is in a collection folder
+      // (a root folder without a '_' prefix)
+      const folderRegex = new RegExp(`^./${config.dir.src}/([^_][^/]+)/.*$`);
+      let matches = data.page.inputPath.match(folderRegex);
+
+      if (matches) {
+        folder = matches[1];
+        if (fs.existsSync(`${config.dir.src}/_layouts/${folder}.njk`)) {
+          layout = folder;
+        }
+      }
+      return layout;
+    },
     permalink: (data) => {
       if (data.permalink) {
         // A permalink has been set in the content Front Matter
