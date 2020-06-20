953c49db2d034b8f5beb823648a008e3b79c1ee9
Nicolas Hoizey <nicolas@hoizey.com>
2020-06-21 00:05:04 +0200

    Change constants names

--- rollup.config.js
+++ rollup.config.js
@@ -10,12 +10,15 @@ import fs from 'fs';
 import crypto from 'crypto';
 const config = require('./pack11ty.config.js');
 
-const SRC_PATH_JS = path.join(config.dir.src, '_assets/js');
-const SRC_PATH_CSS = path.join(config.dir.src, '_assets/sass');
-const HASH_PATH = path.join(config.dir.src, '_data');
+const SRC_DIR = config.dir.src;
+const ASSETS_DIR = config.dir.assets;
+const DIST_DIR = config.dir.dist;
 
-const DEST_PATH_JS = path.join(config.dir.dist, 'js');
-const DEST_PATH_CSS = path.join(config.dir.dist, 'css');
+const JS_SRC = path.join(ASSETS_DIR, 'js');
+const JS_DIST = path.join(DIST_DIR, 'js');
+const CSS_SRC = path.join(ASSETS_DIR, 'sass');
+const CSS_DIST = path.join(DIST_DIR, 'css');
+const HASH = path.join(SRC_DIR, '_data');
 
 const createHashedCssFile = function (folder, srcFile, destFile, styles) {
   // Get the 8 first chars of the md5 hash of these styles
@@ -29,7 +32,7 @@ const createHashedCssFile = function (folder, srcFile, destFile, styles) {
   fs.writeFileSync(path.join(folder, hashedDestFile), styles);
 
   // create or update the JSON file listing hashed CSS files
-  const hashesFile = path.join(HASH_PATH, 'hashes_css.json');
+  const hashesFile = path.join(HASH, 'hashes_css.json');
   let hashes = {};
   if (fs.existsSync(hashesFile)) {
     hashes = JSON.parse(
@@ -60,16 +63,16 @@ const plugins_critical = [
     outputStyle: 'compressed',
     output: function (styles, styleNodes) {
       createHashedCssFile(
-        DEST_PATH_CSS,
+        CSS_DIST,
         'critical.css',
         'critical.[hash].css',
         styles
       );
     },
-    watch: SRC_PATH_CSS,
+    watch: CSS_SRC,
   }),
   entrypointHashmanifest({
-    manifestName: path.join(HASH_PATH, 'hashes_critical.json'),
+    manifestName: path.join(HASH, 'hashes_critical.json'),
   }),
 ];
 
@@ -88,16 +91,16 @@ const plugins_additional_iife = [
     outputStyle: 'compressed',
     output: function (styles, styleNodes) {
       createHashedCssFile(
-        DEST_PATH_CSS,
+        CSS_DIST,
         'additional.css',
         'additional.[hash].css',
         styles
       );
     },
-    watch: SRC_PATH_CSS,
+    watch: CSS_SRC,
   }),
   entrypointHashmanifest({
-    manifestName: path.join(HASH_PATH, 'hashes_additional_iife.json'),
+    manifestName: path.join(HASH, 'hashes_additional_iife.json'),
   }),
 ];
 
@@ -116,15 +119,15 @@ const plugins_additional_es = [
     output: false,
   }),
   entrypointHashmanifest({
-    manifestName: path.join(HASH_PATH, 'hashes_additional_es.json'),
+    manifestName: path.join(HASH, 'hashes_additional_es.json'),
   }),
 ];
 
 export default [
   {
-    input: path.join(SRC_PATH_JS, 'critical.js'),
+    input: path.join(JS_SRC, 'critical.js'),
     output: {
-      dir: DEST_PATH_JS,
+      dir: JS_DIST,
       entryFileNames: '[name]-[format].[hash].js',
       format: 'iife',
       name: 'critical',
@@ -133,9 +136,9 @@ export default [
     plugins: plugins_critical,
   },
   {
-    input: path.join(SRC_PATH_JS, 'additional.js'),
+    input: path.join(JS_SRC, 'additional.js'),
     output: {
-      dir: DEST_PATH_JS,
+      dir: JS_DIST,
       entryFileNames: '[name]-[format].[hash].js',
       format: 'iife',
       name: 'additional',
@@ -144,9 +147,9 @@ export default [
     plugins: plugins_additional_iife,
   },
   {
-    input: path.join(SRC_PATH_JS, 'additional.js'),
+    input: path.join(JS_SRC, 'additional.js'),
     output: {
-      dir: DEST_PATH_JS,
+      dir: JS_DIST,
       entryFileNames: '[name]-[format].[hash].js',
       format: 'es',
       sourcemap: true,
@@ -154,7 +157,7 @@ export default [
     plugins: plugins_additional_es,
   },
   {
-    input: path.join(config.dir.src, 'service-worker.js'),
+    input: path.join(JS_SRC, 'service-worker.js'),
     plugins: [
       resolve(),
       replace({
@@ -176,7 +179,7 @@ export default [
       process.env.NODE_ENV === 'production' && terser(),
     ],
     output: {
-      file: path.join(config.dir.dist, 'service-worker.js'),
+      file: path.join(DIST_DIR, 'service-worker.js'),
       format: 'iife',
     },
   },
