da1228de52e5658dc9aacb3967f86b62cc984b13
Nicolas Hoizey <nicolas@hoizey.com>
2023-05-06 16:10:48 +0200

    Refactor Service Worker even more

--- assets/js/service-worker.js
+++ assets/js/service-worker.js
@@ -1,14 +1,9 @@
-import { precacheAndRoute } from 'workbox-precaching';
 import {
 	offlineFallback,
 	pageCache,
 	staticResourceCache,
 	imageCache,
 } from 'workbox-recipes';
-
-// precache static assets with hashes in filenames
-// See the "serviceworker" npm script and "workbox.config.js" configuration
-precacheAndRoute(self.__WB_MANIFEST);
 
 // Serve pages as network first, with 2 seconds timeout and cache fallback
 // https://developer.chrome.com/docs/workbox/modules/workbox-recipes/#page-cache
@@ -19,13 +14,20 @@ pageCache({
 
 // Serve static assets from immediately from cache, and update (aka "Stale While Revalidate")
 // https://developer.chrome.com/docs/workbox/modules/workbox-recipes/#static-resources-cache
+//
+// Also precache static assets with hashes in filenames
+// See the "serviceworker" npm script and "workbox.config.js" configuration
 staticResourceCache({
-	warmCache: ['./manifest.webmanifest', './images/logo-192px.png'],
+	warmCache: self.__WB_MANIFEST,
 });
 
 // Cache a maximum of 100 images, for 90 days each at most
 // https://developer.chrome.com/docs/workbox/modules/workbox-recipes/#image-cache
-imageCache({ maxEntries: 100, maxAgeSeconds: 60 * 60 * 24 * 90 });
+imageCache({
+	maxEntries: 100,
+	maxAgeSeconds: 60 * 60 * 24 * 90,
+	warmCache: ['./images/logo-192px.png'],
+});
 
 // Provide offline fallbacks for HTML and images
 // https://developer.chrome.com/docs/workbox/modules/workbox-recipes/#offline-fallback
--- workbox.config.js
+++ workbox.config.js
@@ -1,6 +1,10 @@
 module.exports = {
 	globDirectory: '_site',
-	globPatterns: ['./css/additional.*.css', './js/additional-*.*.js'],
+	globPatterns: [
+		'./css/additional.*.css',
+		'./js/additional-*.*.js',
+		'./manifest.webmanifest',
+	],
 	swSrc: '_site/service-worker.js',
 	swDest: '_site/service-worker.js',
 };
