078a569c38c2f8f643f916f1886b670decb1cbd0
Nicolas Hoizey <nicolas@hoizey.com>
2023-05-12 21:33:15 +0200

    Remove dependencies and code to move to the plugin

--- eleventy.config.js
+++ eleventy.config.js
@@ -37,87 +37,8 @@ module.exports = function (eleventyConfig) {
 	const pack11ty = require('eleventy-plugin-pack11ty');
 	eleventyConfig.addPlugin(pack11ty, pack11tyPluginOptions);
 
-	// **********************************************************************************
-	// TODO: move to the Pack11ty plugin
-	// **********************************************************************************
-	const postcss = require('postcss');
-
-	eleventyConfig.addPlugin(require('@11ty/eleventy-plugin-bundle'));
-
-	// https://github.com/11ty/eleventy-plugin-bundle#bundle-sass-with-the-render-plugin
-	// https://www.11ty.dev/docs/languages/custom/#example-add-sass-support-to-eleventy
-	const { EleventyRenderPlugin } = require('@11ty/eleventy');
-	eleventyConfig.addPlugin(EleventyRenderPlugin);
-
-	const sass = require('sass');
-	eleventyConfig.addTemplateFormats('scss');
-
-	// https://www.11ty.dev/docs/languages/custom/#example-add-sass-support-to-eleventy
-	eleventyConfig.addExtension('scss', {
-		outputFileExtension: 'css',
-		compile: async function (inputContent, inputPath) {
-			if (!inputPath.includes('src/assets/')) return;
-			let parsed = path.parse(inputPath);
-			if (parsed.name.startsWith('_')) return;
-
-			return async (data) => {
-				let sassResult = sass.compileString(inputContent, {
-					loadPaths: [parsed.dir || '.', 'src/assets/sass', 'node_modules'],
-					style: 'compressed',
-					sourceMap: true,
-				});
-				// TODO: add sourcemap files generation, see https://github.com/sass/dart-sass/issues/1594#issuecomment-1013208452
-
-				if (data.eleventy.env.runMode === 'build') {
-					// Use PostCSS for Autoprefixer and cssnano when building for production
-					let postCssResult = await postcss([
-						require('autoprefixer'),
-						require('cssnano')({
-							preset: ['default', { discardComments: { removeAll: true } }],
-						}),
-					]).process(sassResult.css, { from: inputPath });
-
-					return postCssResult.css;
-				} else {
-					return sassResult.css;
-				}
-			};
-		},
-	});
-
-	const esbuild = require('esbuild');
-	eleventyConfig.addTemplateFormats('js');
-	eleventyConfig.addExtension('js', {
-		outputFileExtension: 'js',
-		read: false,
-		compile: async function (inputContent, inputPath) {
-			if (!inputPath.includes('src/assets/')) return;
-			// TODO: _site/_11ty shouldn't be created
-			return async (data) => {
-				const output = await esbuild.build({
-					entryPoints: [inputPath],
-					// nodePaths: ['.', 'src/assets/js'],
-					bundle: true,
-					format: 'esm',
-					target: 'es6',
-					minify: data.eleventy.env.runMode === 'build',
-					write: false,
-					external: ['fs'],
-				});
-
-				return output.outputFiles[0].text;
-			};
-		},
-	});
-	// **********************************************************************************
-	// End TODO
-	// **********************************************************************************
-
 	eleventyConfig.setDataDeepMerge(true);
 	eleventyConfig.setQuietMode(true);
-
-	// Ignore JS files in _11ty
-	eleventyConfig.ignores.add('./src/_11ty');
 
 	return {
 		templateFormats: ['md', 'njk'],
