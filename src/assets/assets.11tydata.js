af1a2c3d9214fb0a7a089e50b8f4629df0fb7de5
Nicolas Hoizey <nicolas@hoizey.com>
2024-10-11 18:31:17 +0200

    Add comments

--- src/assets/assets.11tydata.js
+++ src/assets/assets.11tydata.js
@@ -1,9 +1,11 @@
 export default {
 	layout: false,
 	permalink: (data) => {
+		// Only the service worker should generate an actual file
 		if (data.page.filePathStem.endsWith('service-worker')) {
 			return 'service-worker.js';
 		}
+		// Other assets will be inlined
 		return false;
 	},
 };
